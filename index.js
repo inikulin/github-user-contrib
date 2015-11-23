a3499db1810da34ba9c2278f58083487117a2145
inikulin <ifaaan@gmail.com>
2015-11-23 16:27:31 +0300

    Commits stats

--- index.js
+++ index.js
@@ -1,95 +1,162 @@
-var Promise = require('pinkie-promise');
-var cheerio = require('cheerio');
-var chalk   = require('chalk');
-var request = require('request');
-var format  = require('util').format;
+var Promise     = require('pinkie-promise');
+var ProgressBar = require('progress');
+var Table       = require('cli-table');
+var cheerio     = require('cheerio');
+var chalk       = require('chalk');
+var request     = require('request');
+var split       = require('split-array');
+var format      = require('util').format;
 
-var USER_PAGE_URL_TMPL   = 'https://github.com/%s';
-var CONTRIB_DAY_URL_TMPL = 'https://github.com/%s?tab=contributions&from=%s';
-var DAY_SELECTOR         = 'rect.day';
+var USER_PAGE_URL_TMPL     = 'https://github.com/%s';
+var CONTRIB_MONTH_URL_TMPL = 'https://github.com/%s?tab=contributions&from=%s&to=%s';
+
+var DAY_SELECTOR    = 'rect.day';
+var HEADER_SELECTOR = 'h3.conversation-list-heading';
+
+var COMMIT_HEADER_TEXT_RE = /\d+ commits?/;
+var COMMIT_TEXT_RE        = /Pushed (\d+) commits? to (.+)/;
 
 var username = process.argv[2];
 
-var commits      = 0;
-var pullRequests = 0;
-var issues       = 0;
+var commitsTotal      = 0;
+var pullRequestsTotal = 0;
+var issuesTotal       = 0;
 
 var projectStats = {};
 
 function get (url) {
-    return new Promise(function (resolve) {
-        request(url, function (err, res) {
+    return new Promise(function (resolve, reject) {
+        request({
+            url:     url,
+            forever: true
+        }, function (err, res) {
+            if (err)
+                reject(err);
+
+            if (res.statusCode === 429)
+                error('Too many requests to GitHub. Please, wait a minute and try again.');
+
+            if (res.statusCode !== 200)
+                error('GitHub responded with status code ' + res.statusCode);
+
             resolve(res);
         });
     });
 }
 
 function error (text) {
-    console.log(chalk.red.bold('ERROR') + ' ' + text);
+    console.error('\n' + chalk.red.bold('ERROR') + ' ' + text);
     process.exit(1);
 }
 
-function getContributionDayUrls (userPageBody) {
+function getProjectStats (projectName) {
+    projectName = projectName.trim();
+
+    if (!projectStats[projectName]) {
+        projectStats[projectName] = {
+            commits:      0,
+            pullRequests: 0,
+            issues:       0
+        };
+    }
+
+    return projectStats[projectName];
+}
+
+function getMonthContribInfoUrls (userPageBody) {
     var $ = cheerio.load(userPageBody);
 
-    return $(DAY_SELECTOR)
-        .filter(function (idx, el) {
-            var countAttr = $(el).attr('data-count');
-
-            return parseInt(countAttr, 10) > 0;
-        })
+    var days = $(DAY_SELECTOR)
         .map(function (idx, el) {
-            var date = $(el).attr('data-date');
-
-            return format(CONTRIB_DAY_URL_TMPL, username, date);
+            return $(el).attr('data-date');
         })
-        .get();
-}
+        .get()
+        .sort();
 
-function createStatus (total) {
-  // TODO
-}
-
-function fetchDateStats (url, progress) {
-    return get(url)
-        .then(function () {
-            progress.tick();
+    return split(days, 30)
+        .map(function (month) {
+            return format(CONTRIB_MONTH_URL_TMPL, username, month[0], month[month.length - 1]);
         });
 }
 
-function fetchYearStats (userPageBody) {
-    var dayUrls = getContributionDayUrls(userPageBody);
-    var counter = createStatus(dayUrls.length);
+function parseMonthCommits ($) {
+    var $commits = $(HEADER_SELECTOR)
+        .filter(function (idx, el) {
+            var headerText = $(el).text();
 
-    return dayUrls.reduce(function (fetchPromise, url) {
-        return fetchPromise
-            .then(function () {
-                return get(url);
-            })
-            .then(function () {
-                counter.inc();
-            });
-    }, Promise.resolve());
+            return COMMIT_HEADER_TEXT_RE.test(headerText);
+        })
+        .next('ul')
+        .find('li');
+
+    commitsTotal += $commits.length;
+
+    $commits.each(function (idx, el) {
+        var text  = $(el).find('a').text();
+        var match = text.match(COMMIT_TEXT_RE);
+        var stats = getProjectStats(match[2]);
+
+        stats.commits += parseInt(match[1], 10);
+    });
+}
+
+function parseMonthStats (res) {
+    var $ = cheerio.load(res.body);
+
+    parseMonthCommits($);
+}
+
+function fetchYearStats (userPageBody) {
+    var monthUrls = getMonthContribInfoUrls(userPageBody);
+
+    var progress = new ProgressBar('Fetching data: [:bar] :percent', {
+        total: monthUrls.length,
+        width: 50,
+        clear: true
+    });
+
+    var progressTick = progress.tick.bind(progress);
+
+    var monthStatsPromises = monthUrls.map(function (url) {
+        return get(url)
+            .then(parseMonthStats)
+            .then(progressTick);
+    });
+
+    return Promise.all(monthStatsPromises);
+}
+
+function printStats () {
+    var table = new Table({
+        head:      ['Project', 'C', 'PR', 'I'],
+        colWidths: [50, 6, 6, 6]
+    });
+
+    Object.keys(projectStats)
+        .sort()
+        .forEach(function (projectName) {
+            var stats = getProjectStats(projectName);
+
+            table.push([projectName, stats.commits, stats.pullRequests, stats.issues]);
+        });
+
+    console.log(table.toString());
 }
 
 (function run () {
     if (!username)
-        error('you should specify the username');
-
-    console.log('Fetching data...');
+        error('You should specify the username');
 
     var userPageUrl = format(USER_PAGE_URL_TMPL, username);
 
     get(userPageUrl)
         .then(function (res) {
             if (res.statusCode === 404)
-                error('unknown username "' + username + '"');
+                error('Unknown username "' + username + '"');
 
             return fetchYearStats(res.body);
         })
-        .then(function () {
-            console.log('done');
-        })
+        .then(printStats)
         .catch(function (err) {
             error(err.message + '\n' + err.stack);
         });
