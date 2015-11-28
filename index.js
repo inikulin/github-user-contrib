10e298247b3bc7b930932028548b826f63b634d8
inikulin <ifaaan@gmail.com>
2015-11-28 23:49:22 +0300

    CLI: reporters, sorting

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ var moment      = require('moment');
 var repeat      = require('repeat-string');
 var windowSize  = require('window-size');
 var wordwrap    = require('wordwrap');
+var Command     = require('commander').Command;
+var readPkgUp   = require('read-pkg-up').sync;
+var compareFunc = require('compare-func');
 var format      = require('util').format;
 var resolveUrl  = require('url').resolve;
 
 
+// Const
 var GITHUB_URL             = 'https://github.com';
 var COMMITS_URL_TEMPLATE   = 'https://github.com/inikulin/%s/commits?author=%s';
 var CONTRIB_CHUNK_URL_TMPL = 'https://github.com/%s?tab=contributions&from=%s&to=%s';
@@ -28,7 +32,7 @@ var ITEM_STATE_SELECTOR   = 'span.state';
 var COMMIT_HEADER_TEXT_RE       = /\d+ commits?/i;
 var PULL_REQUEST_HEADER_TEXT_RE = /\d+ pull requests?/i;
 var ISSUES_HEADER_TEXT_RE       = /\d+ issues? reported/i;
-var COMMIT_TEXT_RE              = /Pushed (\d+) commits? to (.+)/;
+var COMMIT_TEXT_RE              = /pushed (\d+) commits? to (.+)/i;
 
 var ITEM_STATE_STYLE = {
     open:   chalk.green,
@@ -36,8 +40,23 @@ var ITEM_STATE_STYLE = {
     merged: chalk.blue
 };
 
-var username    = process.argv[2];
-var windowWidth = process.stdout.isTTY ? windowSize.width : 80;
+var WINDOW_WIDTH = process.stdout.isTTY ? windowSize.width : 80;
+
+
+// Program
+var program = new Command('github-user-contrib');
+
+program
+    .version(readPkgUp().pkg.version)
+    .usage('<username> [options]')
+    .option('-r, --reporter <type>', 'specify the reporter type to use (table|verbose|json)', /^(table|verbose|json)$/i, 'table')
+    .option('-s, --sort <by>', 'specify the sorting property (project|commits|pr|issues|total)', /^(project|commits|pr|issues|total)$/i, 'project')
+    .parse(process.argv);
+
+
+// Globals
+var username = program.args[0];
+var opts     = program.opts();
 
 var stats = {
     commitsTotal:      0,
@@ -46,32 +65,8 @@ var stats = {
     projectStats:      {}
 };
 
-function get (url) {
-    return new Promise(function (resolve, reject) {
-        var opts = {
-            url:     url,
-            forever: true
-        };
 
-        request(opts, function (err, res) {
-            if (err)
-                reject(err);
-
-            else if (res.statusCode === 429)
-                reject('Too many requests to GitHub. Please, wait a minute and try again.');
-
-            else if (res.statusCode === 404)
-                reject('Unknown username "' + username + '".');
-
-            else if (res.statusCode !== 200)
-                reject('GitHub responded with status code ' + res.statusCode + '.');
-
-            else
-                resolve(res);
-        });
-    });
-}
-
+// Utils
 function reportError (err) {
     var msg = typeof err === 'string' ? err : err.message + '\n' + err.stack;
 
@@ -97,6 +92,8 @@ function getProjectStats (projectName) {
     return stats.projectStats[projectName];
 }
 
+
+// Parsing
 function getListItems ($, headerTextRe) {
     return $(HEADER_SELECTOR)
         .filter(function (idx, el) {
@@ -162,6 +159,33 @@ function parseChunkStats (res) {
     parseIssues($);
 }
 
+// Fetching
+function get (url) {
+    return new Promise(function (resolve, reject) {
+        var opts = {
+            url:     url,
+            forever: true
+        };
+
+        request(opts, function (err, res) {
+            if (err)
+                reject(err);
+
+            else if (res.statusCode === 429)
+                reject('Too many requests to GitHub. Please, wait a minute and try again.');
+
+            else if (res.statusCode === 404)
+                reject('Unknown username "' + username + '".');
+
+            else if (res.statusCode !== 200)
+                reject('GitHub responded with status code ' + res.statusCode + '.');
+
+            else
+                resolve(res);
+        });
+    });
+}
+
 function getChunkStatsUrls (from, to) {
     var urls       = [];
     var chunkStart = from;
@@ -199,6 +223,38 @@ function fetchStats (from, to) {
     return Promise.all(chunkStatsPromises);
 }
 
+
+// Printing
+function getSortComparer () {
+    switch (opts.sort) {
+        case 'commits':
+            return compareFunc(function (projectName) {
+                return getProjectStats(projectName).commits.count;
+            });
+
+        case 'pr':
+            return compareFunc(function (projectName) {
+                return getProjectStats(projectName).pullRequests.length;
+            });
+
+        case 'issues':
+            return compareFunc(function (projectName) {
+                return getProjectStats(projectName).issues.length;
+            });
+
+        case 'total':
+            return compareFunc(function (projectName) {
+                var projectStats = getProjectStats(projectName);
+
+                return projectStats.commits.count + projectStats.pullRequests.length + projectStats.issues.length;
+            });
+
+        default:
+            return void 0;
+    }
+
+}
+
 function printTable () {
     var table = new Table({
         head:      ['Project', 'Comm', 'PR', 'Iss', 'Total'],
@@ -206,7 +262,7 @@ function printTable () {
     });
 
     Object.keys(stats.projectStats)
-        .sort()
+        .sort(getSortComparer())
         .forEach(function (projectName) {
             var projectStats = getProjectStats(projectName);
             var projectTotal = projectStats.commits.count +
@@ -236,7 +292,7 @@ function printTable () {
 
 function printVariedStateItem (item) {
     var stateStyle = ITEM_STATE_STYLE[item.state];
-    var wrap       = wordwrap(4, windowWidth);
+    var wrap       = wordwrap(4, WINDOW_WIDTH);
 
     console.log(stateStyle('   ' + item.state.toUpperCase()));
     console.log(wrap(chalk.gray(item.title)));
@@ -245,7 +301,7 @@ function printVariedStateItem (item) {
 
 function printVerbose () {
     Object.keys(stats.projectStats)
-        .sort()
+        .sort(getSortComparer())
         .forEach(function (projectName) {
             var projectStats = getProjectStats(projectName);
             var projectTotal = projectStats.commits.count +
@@ -274,9 +330,11 @@ function printVerbose () {
                 projectStats.issues.forEach(printVariedStateItem);
                 console.log();
             }
+
+            console.log();
         });
 
-    console.log(repeat('-', windowWidth - 1));
+    console.log(repeat('-', WINDOW_WIDTH - 1));
 
     var total = stats.commitsTotal + stats.pullRequestsTotal + stats.issuesTotal;
 
@@ -288,10 +346,22 @@ function printVerbose () {
     );
 }
 
-function printJSON () {
-    console.log(JSON.stringify(stats));
+function print () {
+    switch (opts.reporter) {
+        case 'verbose':
+            printVerbose();
+            break;
+
+        case 'json':
+            console.log(JSON.stringify(stats));
+            break;
+
+        default:
+            printTable();
+    }
 }
 
+// Entry point
 (function run () {
     var to   = moment();
     var from = moment(to).subtract(1, 'year');
@@ -301,7 +371,7 @@ function printJSON () {
 
 
     fetchStats(from, to)
-        .then(printVerbose)
+        .then(print)
         .catch(reportError);
 })();
 
